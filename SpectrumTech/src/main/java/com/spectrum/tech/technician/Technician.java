09c3efe4d9890d55299ec49f263bd467eaf1b446
johnordono <john.ordono@gmail.com>
2017-07-25 14:42:18 -0500

    Add files via upload

--- SpectrumTech/src/main/java/com/spectrum/tech/technician/Technician.java
+++ SpectrumTech/src/main/java/com/spectrum/tech/technician/Technician.java
@@ -1,11 +1,15 @@
 package com.spectrum.tech.technician;
 
 
+import com.spectrum.tech.ticket.Ticket;
+
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import java.io.Serializable;
 import java.util.ArrayList;
-import java.util.List;
+import java.util.Arrays;
+
 
 @Entity
 public class Technician implements Serializable {
@@ -15,31 +19,35 @@ public class Technician implements Serializable {
     private String firstName;
     private String lastName;
     private String location;
-    private List<String> assignedTickets = new ArrayList<>(); //List of Ticket id's
-
+    private String[] ticketIds;
 
     public Technician() {
     }
 
-    public Technician(String id, String firstName, String lastName, String location, List<String> tickets){
+    public Technician(String id, String firstName, String lastName, String location, String[] ticketIds){
         this.id = id;
         this.firstName = firstName;
         this.lastName = lastName;
         this.location = location;
-        this.assignedTickets = assignedTickets;
+        this.ticketIds = ticketIds;
     }
 
     public String getId(){ return id; }
     public String getFirstName(){ return firstName; }
     public String getLastName() { return lastName; }
     public String getLocation() { return location; }
-    public List<String> getAssignedTickets() { return assignedTickets; }
+    public String[] getTicketIds(){ return ticketIds; }
 
     public void setId(String id){ this.id = id; }
     public void setFirstName(String firstName){this.firstName = firstName; }
     public void setLastName(String lastName) { this.lastName = lastName; }
     public void setLocation(String location) { this.location = location; }
 
-    public void addAssignedTicket(Ticket ticket){ this.assignedTickets.add(ticket.getId());}
-    public void removeAssignedTicket(String id){ this.assignedTickets.remove(id); }
+    public void addTicketId(String ticketId){
+        int length = this.ticketIds.length;
+        this.ticketIds[length] = ticketId;
+    }
+
+    public void removeTicketId(String id){
+    }
 }
---
+++ SpectrumTech/src/main/java/com/spectrum/tech/ticket/Ticket.java
@@ -0,0 +1,64 @@
+package com.spectrum.tech.ticket;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import java.io.Serializable;
+
+@Entity
+public class Ticket implements Serializable{
+
+    @Id
+    private String id;
+    private String clientName;
+    private String clientAddress;
+    private String clientPhone;
+    private String visitTime;
+    private String status;
+    private String priority;
+    private String problemType;
+    private String description;
+
+    public Ticket(){
+    }
+
+    public Ticket(String id, String clientName, String clientAddress, String clientPhone, String visitTime,
+                    String status, String priority, String problemType, String description){
+        this.id = id;
+        this.clientName = clientName;
+        this.clientAddress = clientAddress;
+        this.clientPhone = clientPhone;
+        this.visitTime = visitTime;
+        this.status = status;
+        this.priority = priority;
+        this.problemType = problemType;
+        this.description = description;
+    }
+
+    public String getId() { return id; }
+    public void setId(String id) { this.id = id; }
+
+    public String getClientName() { return clientName; }
+    public void setClientName(String clientName) { this.clientName = clientName; }
+
+    public String getClientAddress() { return clientAddress; }
+    public void setClientAddress(String clientAddress) { this.clientAddress = clientAddress; }
+
+    public String getClientPhone() { return clientPhone; }
+    public void setClientPhone(String clientPhone) { this.clientPhone = clientPhone; }
+
+    public String getVisitTime() { return visitTime; }
+    public void setVisitTime(String visitTime) { this.visitTime = visitTime; }
+
+    public String getStatus() { return status; }
+    public void setStatus(String status) { this.status = status; }
+
+    public String getPriority() { return priority; }
+    public void setPriority(String priority) { this.priority = priority; }
+
+    public String getProblemType() { return problemType; }
+    public void setProblemType(String problemType) { this.problemType = problemType; }
+
+    public String getDescription(){ return description; }
+    public void setDescription(String description){ this.description = description; }
+
+}
